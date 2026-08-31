296339417bae5e25598b95320295df18e7d2c671
ilsknyaz <ils.knyaz@gmail.com>
2026-08-31 21:44:53 +0600

    add purchase product function

--- homework-7.js
+++ homework-7.js
@@ -1,12 +1,12 @@
-const display_temperature = (city, temp) => {
+const displayTemperature = (city, temp) => {
     console.log(`Сейчас в ${city} температура ${temp} градусов по Цельсию`);
 };
 
-display_temperature("Москва", 40);
+displayTemperature("Москва", 40);
 
 const SPEED_OF_LIGHT = 299792458;
 
-const compare_speed = (speed) => {
+const compareSpeed = (speed) => {
     if (speed > SPEED_OF_LIGHT) {
         console.log("Сверхсветовая скорость");
     } else if (speed < SPEED_OF_LIGHT) {
@@ -16,4 +16,14 @@ const compare_speed = (speed) => {
     }
 }
 
-compare_speed(1000);
+compareSpeed(1000);
+
+let product = "RTX 2060";
+let price = 13900;
+
+const canPurchase = (current_budget) => {
+    let result = current_budget >= price ? `${product} - приобретён` : `Вам не хватает ${price - current_budget} пополните баланс`;
+    console.log(result);
+}
+
+canPurchase(2000);
