25e6c327560f083bc2f0538ae4eb4d805e5bc868
ilsknyaz <ils.knyaz@gmail.com>
2026-08-31 21:34:00 +0600

    add speed compare function

--- homework-7.js
+++ homework-7.js
@@ -1,5 +1,19 @@
-const display_temperature_two = (city, temp) => {
+const display_temperature = (city, temp) => {
     console.log(`Сейчас в ${city} температура ${temp} градусов по Цельсию`);
 };
 
-display_temperature_two("Москва", 40);
+display_temperature("Москва", 40);
+
+const SPEED_OF_LIGHT = 299792458;
+
+const compare_speed = (speed) => {
+    if (speed > SPEED_OF_LIGHT) {
+        console.log("Сверхсветовая скорость");
+    } else if (speed < SPEED_OF_LIGHT) {
+        console.log("Субсветовая скорость");
+    } else {
+        console.log("Скорость света");
+    }
+}
+
+compare_speed(1000);
