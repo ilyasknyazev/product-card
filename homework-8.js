706e58ea205c64befb3d5ef13c434245b4260c2a
ilsknyaz <ils.knyaz@gmail.com>
2026-09-07 11:28:39 +0600

    feat: add user data structure

---
+++ homework-8.js
@@ -0,0 +1,12 @@
+const user = {
+  firstName: "Ilyas",
+  lastName: "Knyazev",
+  age: 24,
+  email: "ilyas.knyazev@example.com",
+  country: "Russia",
+  city: "Omsk",
+  work: "IT company",
+  position: "Frontend developer",
+  maritalStatus: "single",
+  phone: "+7 900 123-45-67",
+};
