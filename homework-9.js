0746c692692c908cbe1be8ef8a5b87db8628c6b5
ilsknyaz <ils.knyaz@gmail.com>
2026-09-14 15:10:17 +0600

    feat: add array filter method

---
+++ homework-9.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const numbersFromFive = numbers.filter(number => number >= 5);
+
+console.log("Исходный массив:", numbers);
+console.log("Массив начиная с 5:", numbersFromFive);
+
