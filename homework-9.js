27620c6493a4193d35b333b6e02b363d72121c71
ilsknyaz <ils.knyaz@gmail.com>
2026-09-14 15:21:56 +0600

    feat: add array includes method

--- homework-9.js
+++ homework-9.js
@@ -5,3 +5,16 @@ const numbersFromFive = numbers.filter(number => number >= 5);
 console.log("Исходный массив:", numbers);
 console.log("Массив начиная с 5:", numbersFromFive);
 
+const books = [
+    "Евгений Онегин",
+    "Преступление и наказание",
+    "Война и мир",
+    "Анна Каренина",
+    "Метель"
+];
+
+const bookExists = books.includes("Война и мир");
+
+console.log("Исходный массив книг:", books);
+console.log("Существует ли книга 'Война и мир'?", bookExists);
+
