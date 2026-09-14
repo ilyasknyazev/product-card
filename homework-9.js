241e7ee79a37f08dbdcb5b5bca6ff4fcbfc364f6
ilsknyaz <ils.knyaz@gmail.com>
2026-09-14 15:25:32 +0600

    feat: add reverse array function

--- homework-9.js
+++ homework-9.js
@@ -1,3 +1,5 @@
+// lvl 1
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const numbersFromFive = numbers.filter(number => number >= 5);
@@ -18,3 +20,11 @@ const bookExists = books.includes("Война и мир");
 console.log("Исходный массив книг:", books);
 console.log("Существует ли книга 'Война и мир'?", bookExists);
 
+const reverseArray = (arr) => {
+    return arr.reverse();
+};
+
+console.log("Перевернутый массив чисел:", reverseArray(numbers));
+console.log("Перевернутый массив книг:", reverseArray(books));
+
+// lvl 2
